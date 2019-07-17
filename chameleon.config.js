1d6ae0eeec62d74a6cc7928b6b56e07cab2b52e7
JiM-W <550116990@qq.com>
2019-07-17 17:13:37 +0800

    cml-flexbox完成测试

--- chameleon.config.js
+++ chameleon.config.js
@@ -3,10 +3,33 @@
 const publicPath = 'https://startheart.github.io/cml-flexbox/dist';
 // 设置api请求前缀
 const apiPrefix = 'https://api.chameleon.com';
-
+const path = require('path')
 cml.config.merge({
   templateLang: "cml",
   templateType: "html",
+  builtinNpmName: 'cml-tt-ui-builtin',
+  extPlatform: {
+    tt: 'cml-tt-plugin',
+  },
+  babelPath: [
+    path.join(__dirname,'node_modules/cml-tt-ui-builtin'),
+    path.join(__dirname,'node_modules/cml-tt-runtime'),
+    path.join(__dirname,'node_modules/cml-tt-api'),
+    path.join(__dirname,'node_modules/cml-tt-ui'),
+    path.join(__dirname,'node_modules/cml-tt-store'),
+    path.join(__dirname,'node_modules/cml-tt-mixins'),
+    path.join(__dirname,'node_modules/mobx'),
+  ],
+  baseStyle:{
+    wx: true,
+    web: true,
+    weex: true,
+    alipay: true,
+    baidu: true,
+    qq: true,
+    tt:true,
+  },
+  // platforms: ['tt'],
   platforms: ["web","weex","wx","alipay","baidu"],
   buildInfo: {
     wxAppId: '123456'
--- dist/wx/static/js/app.js
+++ dist/wx/static/js/app.js
@@ -1,1 +1,59 @@
-var __CML__GLOBAL=require("./manifest.js");__CML__GLOBAL.webpackJsonp([1],{52:function(e,t,n){n(53),n(54)},53:function(e,t){},54:function(e,t,n){function u(e){return e&&e.__esModule?e:{default:e}}function a(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(t,"__esModule",{value:!0});var o=function(){function e(e,t){for(var n=0;n<t.length;n++){var u=t[n];u.enumerable=u.enumerable||!1,u.configurable=!0,"value"in u&&(u.writable=!0),Object.defineProperty(e,u.key,u)}}return function(t,n,u){return n&&e(t.prototype,n),u&&e(t,u),t}}(),r=n(55),f=u(r),l=n(60),i=u(l),d=n(3),c=u(d),s=function(){function e(){a(this,e),this.data={store:f.default,routerConfig:i.default}}return o(e,[{key:"created",value:function(e){}}]),e}();t.default=new s,t.default=c.default.createApp(t.default).getOptions()},55:function(e,t,n){function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=n(56),o=u(a),r=n(57),f=u(r),l=n(58),i=u(l),d=n(59),c=u(d),s=n(12),_=u(s);t.default=(0,_.default)({actions:o.default,getters:f.default,state:i.default,mutations:c.default})},56:function(e,t){Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},57:function(e,t){Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},58:function(e,t){Object.defineProperty(t,"__esModule",{value:!0});var n={};t.default=n},59:function(e,t){Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},60:function(e,t){e.exports={mode:"hash",domain:"https://www.chameleon.com",routes:[{url:"/",path:"/pages/index/index",name:"首页",mock:"index.php"}]}}},[52]);
+var __CML__GLOBAL = require("./manifest.js");
+__CML__GLOBAL.webpackJsonp([22],{
+
+/***/ "../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"/Users/didi/.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/chameleon.js\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=app&media=dev&cmlType=wx&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/app/app.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _chameleonRuntime = __webpack_require__("./node_modules/chameleon-runtime/index.js");
+
+var _chameleonRuntime2 = _interopRequireDefault(_chameleonRuntime);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var App = function () {
+  function App() {
+    _classCallCheck(this, App);
+
+    this.data = {};
+  }
+
+  _createClass(App, [{
+    key: 'created',
+    value: function created(res) {}
+  }]);
+
+  return App;
+}();
+
+exports.default = new App();
+
+
+exports.default = _chameleonRuntime2.default.createApp(exports.default).getOptions();
+
+/***/ }),
+
+/***/ "../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"wx\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"/Users/didi/.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/configs/postcss/wx/.postcssrc.js\"}}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"wx\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=app&media=dev&cmlType=wx&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/app/app.cml":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./src/app/app.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+var __cml__style0 = __webpack_require__("../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"wx\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"/Users/didi/.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/configs/postcss/wx/.postcssrc.js\"}}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"wx\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=app&media=dev&cmlType=wx&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/app/app.cml");
+var __cml__script = __webpack_require__("../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"/Users/didi/.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/chameleon.js\"}!../../../../.nvm/versions/node/v10.7.0/lib/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=app&media=dev&cmlType=wx&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/app/app.cml");
+
+
+/***/ })
+
+},["./src/app/app.cml"]);
